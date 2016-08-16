554c9657b76bdef4da98bb3de6c8ff9612da63c4
pappszi <akihaennem@vipmail.hu>
2016-08-17 00:39:02 +0200

    BT45_CroosProduct

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -16,8 +16,12 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
 	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
 
 	IntendMoveForward(ForwardThrow);
+	IntendTurnRight(RightThrow);
+
+
 	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"),*TankName, *MoveVelocityString);
 }
 
