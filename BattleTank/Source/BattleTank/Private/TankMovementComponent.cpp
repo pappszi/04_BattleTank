cb312070735c3666c9cfaf7e30fe618fb2ef6c89
pappszi <akihaennem@vipmail.hu>
2016-08-17 00:53:45 +0200

    BT46_FinalisingCode

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright SysGames Ltd.
 
 #include "BattleTank.h"
 #include "TankTrack.h"
@@ -15,14 +15,12 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	//no need to call super as we're replacing the functionality
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
 	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
-	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
-
 	IntendMoveForward(ForwardThrow);
-	IntendTurnRight(RightThrow);
 
-
-	//UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"),*TankName, *MoveVelocityString);
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);	
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
@@ -30,7 +28,6 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	//TODO prevent double speed due to dual control use
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw)
@@ -38,6 +35,5 @@ void UTankMovementComponent::IntendTurnRight(float Throw)
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	//TODO prevent double speed due to dual control use
 }
 
