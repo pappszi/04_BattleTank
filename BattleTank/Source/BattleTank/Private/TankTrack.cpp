23b638b90c81ffc77dcde656897633db392ad11c
pappszi <akihaennem@vipmail.hu>
2016-08-16 16:54:47 +0200

    BT40_PhysicsMaterials&Friction

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,5 +9,10 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%s Throttle: %f"),*Name, Throttle);
 
 	//TODO clamp actual throttle value so player cant over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot =Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied,ForceLocation);
+
 }
 
