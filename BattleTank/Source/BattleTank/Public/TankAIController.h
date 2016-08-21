0991c664c84ad29c6cbd8f7c7a1375e4a0ed2ed9
pappszi <akihaennem@vipmail.hu>
2016-08-21 19:30:49 +0200

    BT69_Delegates

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -20,6 +20,12 @@ protected:
 
 private:
 	virtual void BeginPlay() override;
+
+	virtual void SetPawn(APawn* InPawn) override;
+
 	virtual void Tick(float DeltaSeconds) override;
+
+	UFUNCTION()
+	void OnPossessedTankDeath();
 	
 };
